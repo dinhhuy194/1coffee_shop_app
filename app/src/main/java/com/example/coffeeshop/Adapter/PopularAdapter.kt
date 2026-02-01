b9a07840fe26965583405fbda5cc6f31b12e8bce
Huy hoc GIT <phandinhhuy.1904@gmail.com>
2026-02-01 22:44:17 +0700

    feat : product detail

--- app/src/main/java/com/example/coffeeshop/Adapter/PopularAdapter.kt
+++ app/src/main/java/com/example/coffeeshop/Adapter/PopularAdapter.kt
@@ -1,10 +1,12 @@
 package com.example.coffeeshop.Adapter
 
 import android.content.Context
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.example.coffeeshop.Activity.DetailActivity
 import com.example.coffeeshop.Domain.ItemsModel
 import com.example.coffeeshop.databinding.ViewholderPopularBinding
 
@@ -27,7 +29,12 @@ class PopularAdapter(val items:MutableList<ItemsModel>): RecyclerView.Adapter<Po
         Glide.with(context)
             .load(items[position].picUrl[0])
             .into(holder.binding.pic)
+        holder.itemView.setOnClickListener {
+            val intent = Intent(context, DetailActivity::class.java)
+            intent.putExtra("object", items[position])
         }
+
+    }
 
     override fun getItemCount(): Int = items.size
 }
